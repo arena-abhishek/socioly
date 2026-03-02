24216b6b6d865bf415d140aad16cc206837da8e7
arena-abhishek <apdesign0001@gmail.com>
2026-03-02 22:06:08 +0530

    hero section mobile responsivness update

--- components/ui/Stat.tsx
+++ components/ui/Stat.tsx
@@ -1,15 +1,20 @@
 import CountUp from "react-countup";
 
-export default function Stat({ number, label }: { number: number; label: string }) {
+export default function Stat({ number, label }) {
   return (
-    <div className="text-left
-      pr-[clamp(30px,3.42vw,65px)]
-      mr-[clamp(30px,3.42vw,65px)]
-      border-r border-white/20">
-      <span className="block text-[clamp(20px,1.26vw,24px)] font-bold">
+    <div
+      className="
+        text-left
+        md:pr-[clamp(30px,3.42vw,65px)]
+        md:mr-[clamp(30px,3.42vw,65px)]
+        md:border-r md:border-white/20
+      "
+    >
+      <span className="block text-[clamp(1.4rem,4vw,1.8rem)] font-bold leading-none">
         <CountUp end={number} duration={3} separator="," />
       </span>
-      <span className="text-sm text-white/60">
+
+      <span className="text-sm text-white/60 mt-1 block">
         {label}
       </span>
     </div>
